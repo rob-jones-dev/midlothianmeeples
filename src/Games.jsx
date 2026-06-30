712ab784fb1cf98a03cae6a86d03f3d7ff53c1b8
Mr-G-Almighty <rob_jones_92@hotmail.co.uk>
2026-06-30 13:52:57 +0100

    Added functionality for creating games

--- src/Games.jsx
+++ src/Games.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, use } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
@@ -7,6 +7,16 @@ export default function Games() {
     let [games, setGames] = useState([]);
     let [reservationName, setReservationName] = useState("");
     let [reservationError, setReservationError] = useState("");
+    let [addGameName, setAddGameName] = useState("");
+    let [addHostName, setAddHostName] = useState("");
+    let [addGameMinPlayers, setAddGameMinPlayers] = useState(1);
+    let [addGameMaxPlayers, setAddGameMaxPlayers] = useState(1);
+    let [addGameError, setAddGameError] = useState("");
+    let [addGameHostPlaying, setAddGameHostPlaying] = useState(true);
+
+    function capitalizeFirstLetter(val) {
+        return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+    }
 
     useEffect(() => {
     getGames();
@@ -62,6 +72,52 @@ export default function Games() {
         setReservationError("");
     }
 
+    async function addGame() {
+        if (!addGameName.trim()) {
+            setAddGameError("Please enter the name of your game.");
+            return
+        }
+        if (!addHostName.trim()) {
+            setAddGameError("Please enter the name of your Host.");
+            return
+        }
+        if (addGameMinPlayers == 0) {
+            setAddGameError("Please enter a minimum player count higher than 0");
+            return
+        }
+        if (addGameMaxPlayers == 0 || addGameMaxPlayers < addGameMinPlayers) {
+            setAddGameError("Please set a maximum player count higher than 0 and not lower than the minimum player count");
+            return
+        }
+
+        let playersArray = []
+
+        if (addGameHostPlaying && addHostName) {
+            playersArray.push(capitalizeFirstLetter(addHostName));
+        }
+
+        const { data, error } = await supabase.from("games").insert([
+            {
+                name: capitalizeFirstLetter(addGameName),
+                host: capitalizeFirstLetter(addHostName),
+                max_players: addGameMaxPlayers,
+                min_players: addGameMinPlayers,
+                players: playersArray,
+            }
+        ])
+        if (error) {
+            console.error(error)
+        }
+
+        setAddGameError("");
+        getGames();
+        setAddGameName("");
+        setAddHostName("");
+        setAddGameMinPlayers(0);
+        setAddGameMaxPlayers(0);
+        setAddGameHostPlaying(true);
+    }
+
     return(
         <>
             <article>
@@ -94,13 +150,17 @@ export default function Games() {
             <div>
                 <form>
                     <label for="game">Game:</label>
-                    <input name="game" type="text"></input>
+                    <input name="game" type="text" onChange={(e) => setAddGameName(e.target.value)}></input>
                     <label for="host">Host:</label>
-                    <input name="host"></input>
+                    <input name="host" type="text" onChange={(e) => setAddHostName(e.target.value)}></input>
                     <label for="minplayers">Minimum number of players:</label>
-                    <input name="minplayers" type="number"></input>
+                    <input name="minplayers" type="number" value={addGameMinPlayers} onChange={(e) => setAddGameMinPlayers(e.target.value)}></input>
                     <label for="maxplayers">Maximum number of players:</label>
-                    <input name="maxplayers" type="number"></input>
+                    <input name="maxplayers" type="number" value={addGameMaxPlayers} onChange={(e) => setAddGameMaxPlayers(e.target.value)}></input>
+                    <label for="hostPlaying">Will the Host be playing the game?</label>
+                    <input name="hostPlaying" defaultChecked type="checkbox" onChange={(e) => setAddGameHostPlaying(e.target.value)}></input>
+                    <button onClick={addGame} type="button">Submit Game</button>
+                    {addGameError && <p>{addGameError}</p>}
                 </form>
             </div>
         </>
