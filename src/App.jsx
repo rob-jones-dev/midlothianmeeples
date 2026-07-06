1eaaad687e8968f6d9f2e0e879ef265c003ed777
Mr-G-Almighty <rob_jones_92@hotmail.co.uk>
2026-07-06 12:41:36 +0100

    Blurb added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Footer from './Footer.jsx'
 import Games from './Games.jsx'
 import Findus from './Findus.jsx'
 import Reserve from './Reserve.jsx'
+import Blurb from './Blurb.jsx'
 import './App.css'
 
 export default function App() {
   return (
     <>
       <Header />
+      <Blurb />
       <Reserve />
       <Games />
       <Findus />
---
+++ src/Blurb.jsx
@@ -0,0 +1,10 @@
+export default function Blurb() {
+    return (
+        <div className="blurb">
+            <p>Love board games? Join us for a community night of gaming, from card games to board games, fun for all!</p>
+            <p>No games? No problem! Our regulars bring extra games which you can borrow for the evening!</p>
+            <p>Got a favourite you want to share? Bring it along! Maybe even add it below so that others can sign up!</p>
+            <p>Snacks and drinks available for purchase.</p>
+        </div>
+    )
+}
